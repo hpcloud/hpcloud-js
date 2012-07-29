21d5e71128cac604da0ecf261c79466ae5a2aa4d
Matt Butcher <technosophos@gmail.com>
2012-07-28 23:18:09 -0500

    Working on identity services.
    Auth is now successful.

--- test/test-identityservices.js
+++ test/test-identityservices.js
@@ -1,7 +1,26 @@
 var assert = require('assert');
-//var IdentityServices = require('../lib/identityservices');
+var IdentityServices = require('../lib/identityservices');
 var conf = require('./config');
 
 assert.notEqual(0, conf.identity.endpoint.length);
 
-//var idservice = new IdentityServices();
+var idservice = new IdentityServices(conf.identity.endpoint);
+
+var opts = {
+  apiAccessKeyCredentials: {
+    accessKey: conf.identity.account,
+    secretKey: conf.identity.secret
+  },
+  tenantId: conf.identity.tenantid
+}
+idservice.authenticate(opts, function (identity) {
+
+  console.log(identity);
+
+  assert.equal(identity.tenantId(), conf.identity.tenantid);
+  //assert.equal(identity.tenantName(), conf.identity.tenantname);
+  assert.equal(identity.user().name, conf.identity.username);
+  assert.ok(0 < identity.serviceCatalog().length);
+  assert.ok(identity.tenantName().length > 0);
+  assert.ok(identity.token().length > 0);
+});
