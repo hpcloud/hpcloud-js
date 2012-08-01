dfb4cf9c11ca5b7169083bbc4cde68995112c452
Matt Farina <matt@mattfarina.com>
2012-07-31 23:13:21 -0400

    Added methods for authenticating as authenticateAsUser and authenticateAsAccount (with tests).

--- test/test-identityservices.js
+++ test/test-identityservices.js
@@ -30,3 +30,49 @@ idservice.authenticate(opts, function (success, identity) {
   assert.ok(identity.tenantName().length > 0);
   assert.ok(identity.token().length > 0);
 });
+
+/**
+ * Test authenticating as a User.
+ */
+var idservice_testAccountAuth = new IdentityServices(conf.identity.endpoint);
+
+idservice_testAccountAuth.authenticateAsUser(conf.identity.username, conf.identity.password, conf.identity.tenantid, function (success, identity) {
+
+  if (!success) {
+    console.log('Identification failed: %d, %s', arguments[1], arguments[2]);
+    assert.ok(false);
+    return;
+  }
+
+  console.log(identity);
+
+  assert.equal(identity.tenantId(), conf.identity.tenantid);
+  //assert.equal(identity.tenantName(), conf.identity.tenantname);
+  assert.equal(identity.user().name, conf.identity.username);
+  assert.ok(0 < identity.serviceCatalog().length);
+  assert.ok(identity.tenantName().length > 0);
+  assert.ok(identity.token().length > 0);
+});
+
+/**
+ * Test authenticating as a User.
+ */
+var idservice_testUserAuth = new IdentityServices(conf.identity.endpoint);
+
+idservice_testUserAuth.authenticateAsAccount(conf.identity.account, conf.identity.secret, conf.identity.tenantid, function (success, identity) {
+
+  if (!success) {
+    console.log('Identification failed: %d, %s', arguments[1], arguments[2]);
+    assert.ok(false);
+    return;
+  }
+
+  console.log(identity);
+
+  assert.equal(identity.tenantId(), conf.identity.tenantid);
+  //assert.equal(identity.tenantName(), conf.identity.tenantname);
+  assert.equal(identity.user().name, conf.identity.username);
+  assert.ok(0 < identity.serviceCatalog().length);
+  assert.ok(identity.tenantName().length > 0);
+  assert.ok(identity.token().length > 0);
+});
