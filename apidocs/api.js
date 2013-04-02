d540f52290f828dc226c946f68026af1170b4ca9
Matt Farina <matt@mattfarina.com>
2013-04-02 11:52:33 -0400

    Adding api docs in.

---
+++ apidocs/api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ACL",
+        "Container",
+        "Futil",
+        "Identity",
+        "IdentityServices",
+        "ObjectInfo",
+        "RemoteObject",
+        "Subdir",
+        "Transport"
+    ],
+    "modules": [
+        "hpcloud"
+    ],
+    "allModules": [
+        {
+            "displayName": "hpcloud",
+            "name": "hpcloud"
+        }
+    ]
+} };
+});
